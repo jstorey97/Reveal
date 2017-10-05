614a2db68877d50e86a8b866f9499e2623ac3b31
semaaJ <mamesjiles@gmail.com>
2017-10-05 16:54:54 +0100

    Login and Register pages added. CSS/DB functional

--- reveal-app/reveal.py
+++ reveal-app/reveal.py
@@ -5,12 +5,13 @@ from random import choice
 from string import ascii_letters
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, UserMixin, login_required
 from flask import render_template, request, redirect, url_for, flash
-from wtforms import Form, StringField, PasswordField, validators
-from wtforms.fields.html5 import EmailField
-from datetime import datetime
 
+
+from models import RegisterForm, ConfirmationForm, LoginForm
+
+from datetime import datetime
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
@@ -20,17 +21,12 @@ app.secret_key = 'BULKpowders2017'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'users.login'
+login_manager.login_view = 'login'
 
 db = SQLAlchemy(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
 
     fullname = db.Column(db.String(60))
@@ -50,14 +46,14 @@ class User(db.Model):
     registered_at = db.Column(db.DateTime())
 
 
-class RegisterForm(Form):
-    name = StringField('Full Name', [validators.Length(min=1, max=60)])
-    email = EmailField('Email address', [validators.DataRequired(), validators.Email()])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
-    ])
-    confirm = PasswordField('Confirm Password')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/register/', methods=['GET', 'POST'])
@@ -65,41 +61,30 @@ def register():
     form = RegisterForm(request.form)
 
     if request.method == 'POST' and form.validate():
-        fullname = form.name.data
-        email = form.email.data
-        password = sha256_crypt.encrypt(str(form.password.data))
-        verification_phrase = ''.join([choice(ascii_letters) for i in range(11)])
-        current_ip = request.remote_addr
-
-        user = User(fullname=fullname, email=email, password=password,
-                    verification_phrase=verification_phrase, registered_at=datetime.now(),
-                    current_login_ip=current_ip)
+        user = User(fullname=form.name.data,
+                    email=form.email.data,
+                    password=sha256_crypt.encrypt(str(form.password.data)),
+                    verification_phrase=''.join([choice(ascii_letters) for _ in range(11)]),
+                    registered_at=datetime.now(),
+                    current_login_ip=request.remote_addr,
+                    confirmed=False)
 
         db.session.add(user)
         db.session.commit()
 
         return redirect(url_for('confirm_email',
-                                name=fullname,
-                                email=email))
+                                name=form.name.data,
+                                email=form.email.data))
 
     return render_template('register.html',
                            form=form)
 
 
-class Confirmation(Form):
-    code = StringField('Confirmation code', [validators.Length(min=1, max=10)])
-
-
-@app.route('/register/confirm')
+@app.route('/register/confirm',  methods=['GET', 'POST'])
 def confirm_email():
-    form = Confirmation(request.form)
+    form = ConfirmationForm(request.form)
     return render_template('email_confirmation.html',
                            form=form)
-
-
-class LoginForm(Form):
-    email = EmailField('Email address', [validators.DataRequired(), validators.Email()])
-    password = PasswordField('Password')
 
 
 @app.route('/login/', methods=['GET', 'POST'])
@@ -113,13 +98,23 @@ def login():
         user = User.query.filter_by(email=email).first()
 
         if user is not None and sha256_crypt.verify(password, user.password):
-            login_user(user)
+            if not user.confirmed:
+                flash("Please confirm your email")
+
+            else:
+                login_user(user, remember=form.remember)
 
         else:
             flash("Incorrect email or the address is not registered")
 
     return render_template('login.html',
                            form=form)
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__ == "__main__":
