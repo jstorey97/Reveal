bbfba4e4cabb34fb02513d1f46852625ca1c8c00
semaaJ <mamesjiles@gmail.com>
2017-10-09 23:35:58 +0100

    404 added

--- reveal-app/reveal.py
+++ reveal-app/reveal.py
@@ -130,7 +130,7 @@ def login():
 
             else:
                 login_user(user, remember=True)
-                print("Logged in")
+                redirect(url_for('dashboard'))
 
         else:
             flash("Incorrect email or the address is not registered")
@@ -152,5 +152,10 @@ def logout():
     return redirect(url_for(index))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
